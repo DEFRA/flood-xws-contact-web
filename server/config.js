f3d8b5ced834529d8905114810b5eebd1e46bb07
David Stone <davidjamesstone@gmail.com>
2021-12-29 12:14:54 +0000

    Update config environments

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const joi = require('joi')
-const envs = ['local', 'development', 'test', 'production']
+const envs = ['development', 'test', 'production']
 
 const schema = joi.object().keys({
   env: joi.string().valid(...envs).required(),
