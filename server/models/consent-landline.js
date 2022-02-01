ef1671aaf5d42c956bba3d1beac5a768e18e9a4c
David Stone <davidjamesstone@gmail.com>
2022-02-01 17:32:40 +0000

    Update consent landline

---
+++ server/models/consent-landline.js
@@ -0,0 +1,50 @@
+const joi = require('joi')
+const { BaseViewModel, baseMessages } = require('./form')
+
+const PAGE_HEADING = 'Get warnings by telephone call?'
+const CONSENT_KEY = 'consent'
+const CONSENT_LABEL = 'Consent'
+const CONSENT_ITEMS = [true, false]
+const CONSENT_ITEM_LABELS = ['Yes', 'No']
+const CONSENT_MESSAGES = {
+  'any.required': 'Choose an option'
+}
+
+const schema = joi.object().keys({
+  [CONSENT_KEY]: joi.boolean().label(CONSENT_LABEL)
+    .valid(...CONSENT_ITEMS).required().messages(CONSENT_MESSAGES)
+}).messages(baseMessages).required()
+
+class ViewModel extends BaseViewModel {
+  constructor (data, err) {
+    super(data, err, {
+      pageHeading: PAGE_HEADING,
+      path: '/consent-landline'
+    })
+
+    this.addField(CONSENT_KEY, {
+      id: CONSENT_KEY,
+      idPrefix: CONSENT_KEY,
+      name: CONSENT_KEY,
+      fieldset: {
+        legend: {
+          text: PAGE_HEADING,
+          classes: 'govuk-fieldset__legend--l',
+          isPageHeading: true
+        }
+      },
+      classes: 'govuk-radios--inline',
+      items: CONSENT_ITEMS.map((value, index) => ({
+        value: value,
+        text: CONSENT_ITEM_LABELS[index],
+        checked: value === this.data[CONSENT_KEY]
+      })),
+      errorMessage: this.errors.consent
+    })
+  }
+}
+
+module.exports = {
+  schema,
+  ViewModel
+}
--- server/routes/consent-landline.js
+++ server/routes/consent-landline.js
@@ -1,30 +1,15 @@
-const joi = require('joi')
-const BaseModel = require('flood-xws-common/view/model')
-const { getMappedErrors } = require('flood-xws-common/view/errors')
+const { Errors } = require('../models/form')
+const { ViewModel, schema } = require('../models/consent-landline')
 const { updateContactLandlineActive } = require('../lib/db')
-
-const errorMessages = {
-  consent: {
-    '*': 'Choose an option'
-  }
-}
-
-const schema = {
-  consent: joi.boolean().required()
-}
-
-class Model extends BaseModel {
-  constructor (data, err) {
-    super(data, err, errorMessages)
-  }
-}
 
 module.exports = [
   {
     method: 'GET',
     path: '/consent-landline',
     handler: (request, h) => {
-      return h.view('consent-landline', new Model())
+      const { contact } = request.auth.credentials
+
+      return h.view('consent-landline', new ViewModel({ consent: contact.landline_active }))
     }
   },
   {
@@ -47,11 +32,12 @@ module.exports = [
     },
     options: {
       validate: {
-        payload: joi.object().keys(schema),
+        payload: schema,
         failAction: (request, h, err) => {
           const { payload } = request
-          const errors = getMappedErrors(err, errorMessages)
-          return h.view('consent-landline', new Model({ ...payload }, errors)).takeover()
+          const errors = Errors.fromJoi(err)
+
+          return h.view('consent-landline', new ViewModel({ ...payload }, errors)).takeover()
         }
       }
     }
